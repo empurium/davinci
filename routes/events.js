516240008248578ac4294fdeb1fcd8d777d66a49
Michael Simmons <github@e7i.org>
2014-02-18 15:50:25 -0800

    routes/events.js

---
+++ routes/events.js
@@ -0,0 +1,28 @@
+var Config = require('../app/config');
+var mongo  = require('../app/db/mongo');
+
+var events = module.exports = {};
+
+events.events = function(req, res) {
+	if ( ! req.xhr ) { res.render('pictures.html'); }
+
+	mongo.db.collection('events')
+		.find()
+		.limit(50)
+		.sort({ begins: -1 })
+		.toArray(function(err, events) {
+			res.send(events);
+	});
+}
+
+events.event = function(req, res) {
+	var year    = req.params.year;
+	var month   = req.params.month;
+	var slug    = req.params.slug;
+
+	var fullSlug = year + '/' + month + '/' + slug;
+
+	mongo.db.collection('events').findOne({ slug: fullSlug }, function(err, event) {
+		res.send(event);
+	});
+}
