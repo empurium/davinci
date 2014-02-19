7fef1050cc16919dd2e26f55aa998407514979a1
Michael Simmons <github@e7i.org>
2014-02-18 17:25:20 -0800

    Single page basics in place! Still need back/fwd button handling

--- public/js/davinci.js
+++ public/js/davinci.js
@@ -1,3 +1,47 @@
 $(function() {
-	$.ajax()
+	// this initial load should be done with server-side handlebars
+	// templates for the fastest initial load
+	if (top.location.pathname === '/') {
+		$.ajax({
+			url: '/events/recent',
+			success: function(events) {
+				for (i = 0; i < events.length; i++) {
+					$('div#grid-view').append(Handlebars.templates['event-grid'](events[i]));
+				}
+				setupEventThumbs();
+			}
+		});
+	}
+
+	// handle landing on a specific event URL
+	if (top.location.pathname !== '/') {
+		fetchEventPics(top.location.pathname);
+	}
+
+	function fetchEventPics(url) {
+		$.ajax({
+			url: url,
+			success: function(pics) {
+				updateUrl(url);
+				$('div#grid-view').html('');
+				for (i = 0; i < pics.files.length; i++) {
+					$('div#grid-view').append(Handlebars.templates['pic-grid']({
+						slug: pics.slug,
+						thumb: pics.files[i]
+					}));
+				}
+				setupEventThumbs();
+			}
+		});
+	}
+
+	function setupEventThumbs() {
+		$('div#grid-view div').click(function() {
+			fetchEventPics($(this).attr('data-url'));
+		});
+	}
+
+	function updateUrl(url) {
+		window.history.pushState({}, "", url);
+	}
 });
