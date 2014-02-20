7fbec9bc14a1261263f19a474401e56a836cd52a
Michael Simmons <github@e7i.org>
2014-02-20 12:47:35 -0800

    Remove rounded pic edges on EventPics display

--- app/templates/pic-grid.tpl
+++ app/templates/pic-grid.tpl
@@ -1,3 +1,3 @@
 <div class="pic-grid" data-url="{{ slug }}">
-	<img class="img-rounded" src="/thumb/{{ slug }}/{{ thumb }}?size=220x220" />
+	<img src="/thumb/{{ slug }}/{{ thumb }}?size=220x220" />
 </div>
--- public/tpl/pic-grid.js
+++ public/tpl/pic-grid.js
@@ -10,7 +10,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.slug) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.slug); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "\">\n	<img class=\"img-rounded\" src=\"/thumb/";
+    + "\">\n	<img src=\"/thumb/";
   if (helper = helpers.slug) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.slug); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
