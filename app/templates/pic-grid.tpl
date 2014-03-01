bada8b8ca3d59486b3e843e86dfa9ef665b7f706
Michael Simmons <github@e7i.org>
2014-03-01 13:03:41 -0800

    No more square images on pic grid

--- app/templates/pic-grid.tpl
+++ app/templates/pic-grid.tpl
@@ -1,3 +1,3 @@
 <div class="pic-grid">
-	<a class="fancybox" rel="group" href="/thumb/{{ slug }}/{{ thumb }}?size=1024" data-media-full-url="/view/{{ slug }}/{{ thumb }}"><img src="/thumb/{{ slug }}/{{ thumb }}?size=220x220" alt="" /></a>
+	<a class="fancybox" rel="group" href="/thumb/{{ slug }}/{{ thumb }}?size=1024" data-media-full-url="/view/{{ slug }}/{{ thumb }}"><img src="/thumb/{{ slug }}/{{ thumb }}?size=640" alt="" /></a>
 </div>
--- public/tpl/pic-grid.js
+++ public/tpl/pic-grid.js
@@ -30,7 +30,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   if (helper = helpers.thumb) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.thumb); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + "?size=220x220\" alt=\"\" /></a>\n</div>\n";
+    + "?size=640\" alt=\"\" /></a>\n</div>\n";
   return buffer;
   });
 })();
