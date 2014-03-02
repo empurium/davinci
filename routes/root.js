745044e0cab5c5967029e2452a2355e792d916ff
Michael Simmons <github@e7i.org>
2014-03-01 16:27:09 -0800

    New requests to / now start at latest photos

--- routes/root.js
+++ routes/root.js
@@ -4,7 +4,11 @@ var mongo  = require('../app/db/mongo');
 var root = module.exports = {};
 
 root.root = function(req, res) {
-	if ( ! req.xhr ) { res.render('pictures.html'); return; }
+	if ( ! req.xhr ) {
+		req.session.lastEventBegins = new Date();
+		res.render('pictures.html');
+		return;
+	}
 
 //	if (req.session && req.session.user_id) {
 //	} else {
