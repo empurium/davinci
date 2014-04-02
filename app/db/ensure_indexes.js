43316381f9c6ab8b23b07f7596a547bf8840d47a
Michael Simmons <github@e7i.org>
2014-04-02 09:35:39 -0700

    give output about indexes

--- app/db/ensure_indexes.js
+++ app/db/ensure_indexes.js
@@ -1,18 +1,17 @@
 var mongo = require('./mongo');
 
-var noop   = function() {};
+var out   = function(err, status) { if (err) throw err; console.log(status); };
 var events = mongo.db.collection('events');
 
 console.log("Adding indexes:");
 
 console.log(" - events collection");
-events.ensureIndex({ name: 1 }, noop);
-events.ensureIndex({ slug: 1 }, { unique: true }, noop);
-events.ensureIndex({ year: 1 }, noop);
-events.ensureIndex({ month: 1 }, noop);
-events.ensureIndex({ begins: 1 }, noop);
-events.ensureIndex({ ends: 1 }, noop);
-events.ensureIndex({ path: 1 }, { unique: true }, noop);
+events.ensureIndex({ name: 1 }, out);
+events.ensureIndex({ slug: 1 }, { unique: true }, out);
+events.ensureIndex({ year: 1 }, out);
+events.ensureIndex({ month: 1 }, out);
+events.ensureIndex({ begins: 1 }, out);
+events.ensureIndex({ ends: 1 }, out);
+events.ensureIndex({ path: 1 }, { unique: true }, out);
 
 
-process.exit();
