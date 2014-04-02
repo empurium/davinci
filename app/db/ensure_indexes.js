60abbca1035ddeb0a4584214080cc520a3cabc84
Michael Simmons <github@e7i.org>
2014-04-02 09:32:05 -0700

    Fixing ensure_indexes so it actually works

--- app/db/ensure_indexes.js
+++ app/db/ensure_indexes.js
@@ -1,17 +1,18 @@
+var mongo = require('./mongo');
 
-var mongo  = require('./mongo');
+var noop   = function() {};
 var events = mongo.db.collection('events');
 
 console.log("Adding indexes:");
 
 console.log(" - events collection");
-events.ensureIndex({ name: 1 });
-events.ensureIndex({ slug: 1 }, { unique: true });
-events.ensureIndex({ year: 1 });
-events.ensureIndex({ month: 1 });
-events.ensureIndex({ begins: 1 });
-events.ensureIndex({ ends: 1 });
-events.ensureIndex({ path: 1 }, { unique: true });
+events.ensureIndex({ name: 1 }, noop);
+events.ensureIndex({ slug: 1 }, { unique: true }, noop);
+events.ensureIndex({ year: 1 }, noop);
+events.ensureIndex({ month: 1 }, noop);
+events.ensureIndex({ begins: 1 }, noop);
+events.ensureIndex({ ends: 1 }, noop);
+events.ensureIndex({ path: 1 }, { unique: true }, noop);
 
 
 process.exit();
